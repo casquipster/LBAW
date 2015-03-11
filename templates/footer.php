881e36bdb0ab7c8e594e3b48f7f1f6ec2f73f5dd
Casquipster <joaosoaresacs@gmail.com>
2015-03-11 12:33:58 +0000

    mestre do design

---
+++ templates/footer.php
@@ -0,0 +1,7 @@
+<div id = "developed" class = "navbar navbar-fixed-bottom">
+			<div class = "container">
+				<h4><a href="http://www.fe.up.pt">FEUP</a> LBAW <a href="http://lbaw.fe.up.pt/201415/doku.php/lbaw1464/index">1464</a></h4>
+			</div>
+</div>
+</body>
+</html>
