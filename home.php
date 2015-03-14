66a69d1179bb1ff3ac5746eeddb7ac5cec280a5d
Mafalda Falcão <mafaldafalcaotvf@gmail.com>
2015-03-14 20:31:58 +0000

    Started homepage

---
+++ home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>STOCKR</title>
+  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script> <!-- for jquery-->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+  <!--<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" />-->
+  <link rel="stylesheet" href="templates/style.css" type="text/css">
+  <meta charset = "UTF-8">
+</head>
+
+<style>
+body {
+  display: center;
+  background-image: url("templates/images/background.jpg");
+  background-color: #cccccc;
+}
+</style>
+<body>
+ <!--   <div class="container">
+    <div class="row">
+        <div class="col-lg-9">
+          <div class="jumbotron text-center">
+              <h1>Welcome to Stockr!</h1>
+              <a href="create_poll.php" class="btn btn-info">Create New Poll</a>
+              <a href="search.php" class="btn btn-info">Search</a>
+          </div>
+        </div>
+-->
+
+    <?php include_once('templates/footer.php'); ?>
