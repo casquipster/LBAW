e60ee0fe46fa7acc10e163a74c1755a9ac1f07bb
Mafalda Falcão <mafaldafalcaotvf@gmail.com>
2015-03-16 15:33:33 +0000

    home - changed some details

--- home.php
+++ home.php
@@ -25,4 +25,11 @@ body {
     <div>
   </div>
 
-<?php include_once('templates/footer.php'); ?>
+<div id = "footer1" class = "navbar navbar-fixed-bottom">
+      <div class = "container">
+        <h4><a href="http://www.fe.up.pt">FEUP</a> LBAW <a href="http://lbaw.fe.up.pt/201415/doku.php/lbaw1464/index">1464</a></h4>
+      </div>
+  </div>
+
+</body>
+</html>
