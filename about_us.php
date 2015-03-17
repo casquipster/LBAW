f68094290137c86c48ea404723da42331aa454e1
Mafalda Falcão <mafaldafalcaotvf@gmail.com>
2015-03-17 00:19:14 +0000

    about us - almost done (temporary icons)

---
+++ about_us.php
@@ -0,0 +1,80 @@
+<?php include_once('templates/header.php'); ?>
+
+<section id="team">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h1 id="aboutus" class="section-heading">About Us</h2>
+                    <h3 id="auh3" class="section-subheading text-muted">This is our amazing team!</h3>
+                </div>
+            </div>
+            <h1></h1>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="team-member">
+                        <img src="templates/images/ad" class="img-responsive img-circle" alt="">
+                        <h4>Anaís Dias</h4>
+                        <ul class="list-inline social-buttons">
+                             <li><a href="https://www.facebook.com/anais.s.dias" title="Facebook" class="btn btn-social-icon btn-facebook">
+                            	<i class="glyphicon glyphicon-user"></i></a>
+                            </li>
+                            <li><a href="https://github.com/AnaisDias" title="GitHub">
+                            	<i class="glyphicon glyphicon-folder-open"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="team-member">
+                        <img src="templates/images/js.jpg" class="img-responsive img-circle" alt="">
+                        <h4>João Soares</h4>
+                        <ul class="list-inline social-buttons">
+                             <li><a href="https://www.facebook.com/joao.soares.3781"title="Facebook" class="btn btn-social-icon btn-facebook">
+                            	<i class="glyphicon glyphicon-user"></i></a>
+                            </li>
+                            <li><a href="https://github.com/casquipster" title="GitHub">
+                            	<i class="glyphicon glyphicon-folder-open"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="team-member">
+                        <img src="templates/images/jr.jpg" class="img-responsive img-circle" alt="">
+                        <h4>João Ramos</h4>
+                        <ul class="list-inline social-buttons">
+                           <li><a href="https://www.facebook.com/joao.ramos.39"title="Facebook" class="btn btn-social-icon btn-facebook">
+                            	<i class="glyphicon glyphicon-user"></i></a>
+                            </li>
+                            <li><a href="https://github.com/Joao-Ramos" title="GitHub">
+                            	<i class="glyphicon glyphicon-folder-open"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="team-member">
+                        <img src="templates/images/mf.jpg" class="img-responsive img-circle" alt="">
+                        <h4>Mafalda Falcão</h4>
+                        <ul class="list-inline social-buttons">
+                             <li><a href="https://www.facebook.com/mafaldafalcaotvf"title="Facebook" class="btn btn-social-icon btn-facebook">
+                            	<i class="glyphicon glyphicon-user"></i></a>
+                            </li>
+                            <li><a href="https://github.com/mafaldafalcaotvf" title="GitHub">
+                            	<i class="glyphicon glyphicon-folder-open"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+         </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                	<p></p>
+                    <p class="large text-muted">To know more about our work, visit our <a href="http://lbaw.fe.up.pt/201415/doku.php/lbaw1464/index">Wiki page</a>.</p>
+                </div>
+            </div>
+        </div>
+    </section> 
+
+<?php include_once('templates/footer.php'); ?>
